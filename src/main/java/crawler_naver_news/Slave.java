2346e20ec21ce2920457a9d7d5e9e514625fac47
Insu Kim <usa6463@gmail.com>
2019-07-28 20:45:31 +0900

    send kafka method

--- src/main/java/crawler_naver_news/Slave.java
+++ src/main/java/crawler_naver_news/Slave.java
@@ -29,15 +29,84 @@ import java.nio.file.Paths;
 import java.io.File;
 import com.opencsv.*;
 
+// kafka
+import org.apache.kafka.clients.admin.*;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.*;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.Collections;
+import java.util.Arrays;
+import org.json.simple.*;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
 public class Slave implements Runnable  {
     Scheduler scheduler;
     String href;
     String file;
+    final static String TOPIC_NAME = "naver_news";
+    final static String KAFKA_ADDR = "192.168.0.22:9092";
 
     public Slave(Scheduler scheduler, String href){
         this.scheduler = scheduler;
         this.href = href;
         this.file = "D:\\news.csv";
+    }
+
+    public void send_kafka(String message){
+        // topic check 
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", KAFKA_ADDR);
+        properties.put("connections.max.idle.ms", 10000);
+        properties.put("request.timeout.ms", 5000);
+
+        try (AdminClient client = KafkaAdminClient.create(properties))
+        {
+            ListTopicsResult topics = client.listTopics();
+            Set<String> names = topics.names().get();
+            if (!names.contains(TOPIC_NAME))
+            {
+                System.out.println("no topic what we want");
+                // Define topic
+                // java.lang.String name, int numPartitions, short replicationFactor
+                NewTopic newTopic = new NewTopic(TOPIC_NAME, 1, (short)1);
+
+                // Create topic, which is async call.
+                final CreateTopicsResult createTopicsResult = client.createTopics(Collections.singleton(newTopic));
+                System.out.println("create new topic");
+            }
+            else{
+                System.out.println(names);
+            }
+        }
+        catch (InterruptedException | ExecutionException e)
+        {
+            // Kafka is not available
+            System.out.println("Kafka is not available");
+        }
+
+        // producer start
+        Properties prop = new Properties();
+        prop.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_ADDR);
+        prop.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        prop.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(prop);
+        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, message);
+
+        try {
+            producer.send(record, (metadata, exception) -> {
+                if (exception != null) {
+                    // some exception
+                }
+            });
+        } catch (Exception e) {
+            // exception
+        } finally {
+            producer.flush();
+        }
     }
 
     public boolean news_check(String url){
